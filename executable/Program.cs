34362157cdf120e56736a19fa273bb0ee6a46de4
Michal Barczyk <michal.barczyk@sonarsource.com>
2018-02-16 12:45:39 +0100

    code

---
+++ executable/Program.cs
@@ -0,0 +1,18 @@
+﻿using standard;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace executable
+{
+    class ProgramNetFw
+    {
+        static void Main(string[] args)
+        {
+            var str = new ClassStd().Do();
+            Console.WriteLine(str);
+        }
+    }
+}
---
+++ standard/Class1.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+
+namespace standard
+{
+    public class ClassStd
+    {
+
+        public string Do()
+        {
+            string jsonString = @"{ 'status': 'all fine!' }";
+            var json = JObject.Parse(jsonString);
+            return json["status"].ToString();
+        }
+
+    }
+}
